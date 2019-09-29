c61d84472cd71de6b6598a5ad8760fb1269cc939
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2019-09-29 15:26:50 -0300

    Sprites e tiles opensource

--- Classes/Map.js
+++ Classes/Map.js
@@ -111,10 +111,10 @@ Map.prototype.desenhar = function (ctx) {
     for (var c = Math.max(0,player.sprite.gx-MAPA_AREA); c < Math.min(this.w, player.sprite.gx+MAPA_AREA); c++) {
       switch(this.cell[l][c]){
         case 0:   //Vazio
-          imageLibrary.drawSize(ctx, "sandGround", c*this.s, l*this.s, this.s, this.s);
+          imageLibrary.drawSize(ctx, "floor_sand", c*this.s, l*this.s, this.s, this.s);
           break;
         case 1:   //Bloqueado
-          imageLibrary.drawSize(ctx, "brickRed", c*this.s, l*this.s, this.s, this.s);
+          imageLibrary.drawSize(ctx, "brick_gray", c*this.s, l*this.s, this.s, this.s);
           break;
         case 2:   //Local de saida
           ctx.strokeStyle = "darkBlue";
@@ -127,14 +127,14 @@ Map.prototype.desenhar = function (ctx) {
           ctx.strokeStyle = "Yellow";
           ctx.fillStyle = "orange";
           ctx.linewidth = 10;
-          imageLibrary.drawSize(ctx, "sandGround", c*this.s, l*this.s, this.s, this.s);
+          imageLibrary.drawSize(ctx, "floor_sand", c*this.s, l*this.s, this.s, this.s);
           ctx.save();
           ctx.globalAlpha = 0.40;         //Transparência
           ctx.fillRect(c*this.s, l*this.s, this.s, this.s);
           ctx.strokeRect(c*this.s, l*this.s, this.s, this.s);
           ctx.restore();
         case 4:   //Tesouro a pegar
-          imageLibrary.drawSize(ctx, "sandGround", c*this.s, l*this.s, this.s, this.s);
+          imageLibrary.drawSize(ctx, "floor_sand", c*this.s, l*this.s, this.s, this.s);
           ctx.fillStyle = "yellow";
           ctx.strokeStyle = "grey";
           ctx.fillRect(c*this.s + this.s/3, l*this.s + this.s/3, this.s/3, this.s/3);
@@ -145,14 +145,14 @@ Map.prototype.desenhar = function (ctx) {
           imageLibrary.drawClipSize(ctx, "rockBlock", 0, 0, 32, 32, c*this.s, l*this.s, this.s, this.s);
           break;
         case 6:   //Terreno com mina
-          imageLibrary.drawSize(ctx, "sandGround", c*this.s, l*this.s, this.s, this.s);
-          imageLibrary.drawClipSize(ctx, "sandBlock", 0, 0, 80, 80, c*this.s, l*this.s, this.s, this.s);
+          //imageLibrary.dawSize(ctx, "sandGround", c*this.s, l*this.s, this.s, this.s);
+          //imageLibrary.drawClipSize(ctx, "sandBlock", 0, 0, 80, 80, c*this.s, l*this.s, this.s, this.s);
           break;
         case 7:   //Teleporte
           ctx.strokeStyle = "darkGreen";
           ctx.fillStyle = "lightGreen";
           ctx.linewidth = 10;
-          imageLibrary.drawSize(ctx, "sandGround", c*this.s, l*this.s, this.s, this.s);
+          //imageLibrary.drawSize(ctx, "sandGround", c*this.s, l*this.s, this.s, this.s);
           ctx.save();
           ctx.globalAlpha = 0.40;         //Transparência
           ctx.fillRect(c*this.s, l*this.s, this.s, this.s);
@@ -163,7 +163,7 @@ Map.prototype.desenhar = function (ctx) {
           ctx.strokeStyle = "purple";
           ctx.fillStyle = "purple";
           ctx.linewidth = 10;
-          imageLibrary.drawSize(ctx, "sandGround", c*this.s, l*this.s, this.s, this.s);
+          //imageLibrary.drawSize(ctx, "sandGround", c*this.s, l*this.s, this.s, this.s);
           ctx.save();
           ctx.globalAlpha = 0.60;         //Transparência
           ctx.fillRect(c*this.s, l*this.s, this.s, this.s);
@@ -171,8 +171,8 @@ Map.prototype.desenhar = function (ctx) {
           ctx.restore();
           break;
         case 9:   //Bloqueio com espinho
-          imageLibrary.drawSize(ctx, "sandGround", c*this.s, l*this.s, this.s, this.s);
-          imageLibrary.drawSize(ctx, "tijoloEspinho", c*this.s, l*this.s, this.s, this.s);
+          //imageLibrary.drawSize(ctx, "sandGround", c*this.s, l*this.s, this.s, this.s);
+          //imageLibrary.drawSize(ctx, "tijoloEspinho", c*this.s, l*this.s, this.s, this.s);
           break;
         case 10:  //Posição do inimigo    
           ctx.strokeStyle = "darkgrey";
