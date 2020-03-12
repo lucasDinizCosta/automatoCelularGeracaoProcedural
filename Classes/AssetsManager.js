896b1bd85c584aaf39a87428c471eb0739c9d8d3
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2020-03-12 20:26:21 -0300

    Funções auxiliares de desenho adicionadas na AssetManager

--- Classes/AssetsManager.js
+++ Classes/AssetsManager.js
@@ -35,7 +35,6 @@ AssetsManager.prototype.progresso = function () {
     if (this.aCarregar != 0) {
         return this.carregadas / this.aCarregar * 100.0;
     } else return 0.0;
-
 }
 
 AssetsManager.prototype.loadAudio = function (key, url) {
@@ -67,4 +66,42 @@ AssetsManager.prototype.play = function (key) {
         }
 
     }
+}
+
+/**
+ * FUNÇÕES DE DESENHOS AUXILIARES:
+ */
+
+AssetsManager.prototype.draw = function (ctx, key, x, y) {
+    ctx.drawImage(this.images[key], x, y);
+};
+
+AssetsManager.prototype.drawSize = function (ctx, key, x, y, w, h) {
+    ctx.drawImage(this.images[key], x, y, w, h);
+};
+
+AssetsManager.prototype.drawAngle = function(ctx, key, x, y, ang){
+    ctx.save();
+    ctx.translate(x, y);
+    ctx.rotate(ang * Math.PI/180);
+    ctx.drawImage(this.images[key], -this.images[key].width/2, -this.images[key].height/2);
+    ctx.restore();
+}
+
+AssetsManager.prototype.drawClip = function(ctx, key, sx, sy, w, h, dx, dy){
+    ctx.drawImage(this.images[key], sx, sy, w, h, dx, dy, w, h);
+}
+
+AssetsManager.prototype.drawClipSize = function(ctx, key, sx, sy, w, h, dx, dy, dw, dh){
+    //sx, sy, w, h dentro da imagem - w e h são para a parte da imagem que irá pegar
+    //dx, dy, dw, dh onde irá desenhar e em que proporção
+    ctx.drawImage(this.images[key], sx, sy, w, h, dx, dy, dw, dh);
+}
+
+AssetsManager.prototype.drawClipAngle = function(ctx, key, sx, sy, w, h, dx, dy, ang){
+    ctx.save();
+    ctx.translate(dx, dy);
+    ctx.rotate(ang * Math.PI/180);
+    ctx.drawImage(this.images[key], sx, sy, w, h, -w/2, -h/2, w, h);
+    ctx.restore();
 }
