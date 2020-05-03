561e0d5c757d22d6c75635c178735306df28fc9e
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2020-05-03 15:43:36 -0300

    Refatoração na animação do player e ataque com cooldown

--- Classes/Player.js
+++ Classes/Player.js
@@ -20,7 +20,8 @@ function Player(params) {
     room: -1,
     tesourosColetados: 0,
     playerVel: 180, // 100
-    cooldownTeleporte: 1,   
+    cooldownTeleporte: 1,
+    cooldownAtaque: 1,                  //Tempo do personagem travado até terminar o ataque            
 
     // Mapa das teclas pressionadas
     teclas: {
@@ -40,7 +41,6 @@ function Player(params) {
     poseAtual: 0,
     animation: [],
     numAnimacoes: 8,
-    cooldown: 5,                  //Tempo do personagem travado até terminar o ataque            
     nomeImagem: "player"
   }
 
@@ -53,32 +53,84 @@ function Player(params) {
 Player.prototype = new Sprite();              // Define que o Player é um Sprite
 Player.prototype.constructor = Player;
 
+Player.prototype.restart = function(){
+  this.vivo = true;
+  this.tesourosColetados = 0;
+  this.setRoom();
+}
+
+Player.prototype.setRoom = function(){
+  this.room = this.map.cell[this.gy][this.gx].room;
+}
+
+Player.prototype.moverCompleto = function(dt){
+  this.cooldownTeleporte = this.cooldownTeleporte - dt;         // Cooldown de teleporte pra não teleportar direto
+  this.tratarAnimacao();
+  this.controlePorTeclas();
+  if(this.cooldownAtaque < 0){
+    this.mover(dt);
+  }
+  this.animationController();
+}
+
 Player.prototype.criarAnimacoes = function(){
+  let typeNames = ["Walk", "Atack"];
+
   // Cria a lista de tipos de animações
-  /*for(let i = 0; i < this.qtdAnimacoes.types; i++){
+  for(let i = 0; i < typeNames.length; i++){
     let auxAnimation = {
-        animationFrame: [],
-        type: i,
-        qtdFrames: this.qtdAnimacoes.qtd[i]
+        animationPosition: [],
+        type: typeNames[i],
+        qtdPositions: 4
     }
+    if(i === 0){
+      for(let j = 0; j < 4; j++){             //  4 Direções
+        let auxAnimationPosition = {
+          animationFrame: [],
+          speedAnimation: 10,  //160
+          qtdAnimacoes: 9,
+          sw: 64,
+          sh: 64,
+          sxBegin: 0,
+          syBegin: (8 + j) * 64,
+        }
+        auxAnimation.animationPosition.push(auxAnimationPosition);
+      }
+    }
+    else{
+      for(let j = 0; j < 4; j++){             //  4 Direções
+        let auxAnimationPosition = {
+          animationFrame: [],
+          speedAnimation: 10,
+          qtdAnimacoes: 6,
+          sw: 64,
+          sh: 64,
+          sxBegin: 0,
+          syBegin: (12 + j) * 64,
+        }
+        auxAnimation.animationPosition.push(auxAnimationPosition);
+      }
+    }  
     this.animation.push(auxAnimation);
   }
 
-  for(let i = 0; i < this.animation.length; i++){             // Animações
-      for(let j = 0; j < this.animation[i].qtdFrames; j++){   // Frames
-          let animationFrame = {
-              sizeImagem: this.s,
-              pose: j,
-              sx: 1 + 23 * j,
-              sy: 1 + 23 * this.qtdAnimacoes.lines[i],
-          };
-          this.animation[i].animationFrame.push(animationFrame);
+  for(let i = 0; i < this.animation.length; i++){                 //Walk, Atack
+    let auxAnimation = this.animation[i].animationPosition;
+    for(let j = 0; j < auxAnimation.length; j++){  //4 posições
+      let posicao = auxAnimation[j];
+      for(let k = 0; k < posicao.qtdAnimacoes; k++){            //Monta o vetor de frames de animação
+        let auxAnimationFrame = {
+          sx: posicao.sxBegin + (posicao.sw * k),
+          sy: posicao.syBegin,
+          sw: posicao.sw,
+          sh: posicao.sh,
+        };
+        posicao.animationFrame.push(auxAnimationFrame);
       }
-  }*/
+    }
+  }
 
-
-
-  for(let i = 0; i < this.numAnimacoes; i++){
+  /*for(let i = 0; i < this.numAnimacoes; i++){
     let animationFrame = {
       sizeImagem: 64,
       pose: 0,
@@ -126,38 +178,18 @@ Player.prototype.criarAnimacoes = function(){
   this.animation[7].pose = 15;
   this.animation[7].qtdAnimacoes = 6;
   this.animation[7].typeAnimation = 1;
-  this.animation[7].speedAnimation = 160;
-}
-
-Player.prototype.restart = function(){
-  this.vivo = true;
-  this.tesourosColetados = 0;
-  this.setRoom();
-}
-
-Player.prototype.setRoom = function(){
-  this.room = this.map.cell[this.gy][this.gx].room;
-}
-
-Player.prototype.moverCompleto = function(dt){
-  this.cooldownTeleporte = this.cooldownTeleporte - dt;         // Cooldown de teleporte pra não teleportar direto
-  this.controlePorTeclas();
-  this.tratarAnimacao();
-  //if(this.cooldown < 0){
-    this.mover(dt);
-  //}
-  this.animationController();
+  this.animation[7].speedAnimation = 160;*/
 }
 
 Player.prototype.controlePorTeclas = function(){
   // Teclas direcionais
-  if(this.teclas.up){this.vy = - this.playerVel; this.sentidoMovimento = 3;}
-  if(this.teclas.right){this.vx = this.playerVel; this.sentidoMovimento = 0;}
-  if(this.teclas.down){this.vy = this.playerVel; this.sentidoMovimento = 1;}
-  if(this.teclas.left){this.vx = - this.playerVel; this.sentidoMovimento = 2;}
+  if(this.teclas.up){this.vy = - this.playerVel; this.sentidoMovimento = 0;}
+  if(this.teclas.right){this.vx = this.playerVel; this.sentidoMovimento = 3;}
+  if(this.teclas.down){this.vy = this.playerVel; this.sentidoMovimento = 2;}
+  if(this.teclas.left){this.vx = - this.playerVel; this.sentidoMovimento = 1;}
 
   // Teclas com ações a mais
-  if(this.teclas.ctrl){this.atacando = 1; } else{ this.atacando = 0;}
+  if(this.teclas.ctrl){this.atacando = 1; this.cooldownAtaque = 1;} //else{ this.atacando = 0;}
   if(this.teclas.shift){this.playerVel = 250;  /* 180*/} else {this.playerVel = 180}
   //if(this.teclas.space){this.vx = -playerVel; this.sentidoMovimento = 2;}
 
@@ -170,22 +202,22 @@ Player.prototype.controlePorTeclas = function(){
 Player.prototype.tratarAnimacao = function(){
   switch (this.sentidoMovimento) {  //Movimento
     case 0:     //Direita
-      this.estadoAnimacaoAtual = 3;
+      //this.estadoAnimacaoAtual = 3;
       this.hitBox.x = this.x + this.w/2;
       this.hitBox.y = this.y;
       break;
     case 1:     //Baixo
-      this.estadoAnimacaoAtual = 2;
+      //this.estadoAnimacaoAtual = 2;
       this.hitBox.x = this.x;
       this.hitBox.y = this.y + this.h/2;
       break;
     case 2:     //Esquerda
-      this.estadoAnimacaoAtual = 1;
+      //this.estadoAnimacaoAtual = 1;
       this.hitBox.x = this.x - this.w/2;
       this.hitBox.y = this.y;
       break;
     case 3:     //Cima
-      this.estadoAnimacaoAtual = 0;
+      //this.estadoAnimacaoAtual = 0;
       this.hitBox.x = this.x;
       this.hitBox.y = this.y - this.h/2;
       break;
@@ -203,19 +235,74 @@ Player.prototype.tratarAnimacao = function(){
       break;
   }
 
-  this.sizeImagem = this.animation[this.estadoAnimacaoAtual].sizeImagem;
+  /*this.sizeImagem = this.animation[this.estadoAnimacaoAtual].sizeImagem;
   this.qtdAnimacoes = this.animation[this.estadoAnimacaoAtual].qtdAnimacoes;
   this.pose = this.animation[this.estadoAnimacaoAtual].pose;
   this.typeAnimation = this.animation[this.estadoAnimacaoAtual].typeAnimation;
-  this.speedAnimation = this.animation[this.estadoAnimacaoAtual].speedAnimation;
+  this.speedAnimation = this.animation[this.estadoAnimacaoAtual].speedAnimation;*/
+  this.speedAnimation = this.animation[this.atacando].animationPosition[this.sentidoMovimento].speedAnimation;
+  
+}
+
+Player.prototype.animationController = function(){
+  /*if(this.typeAnimation == 0){
+    if(this.vx !== 0 || this.vy !== 0){
+      this.frameTimeAnimation = this.frameTimeAnimation - this.speedAnimation*dt;
+      if(this.frameTimeAnimation < 0){
+        this.frameTimeAnimation = 12;
+        this.animationState = this.animationState + 1;
+      }
+    }
+    else{
+      this.frameTimeAnimation = 12;
+      this.animationState = 0;
+    }
+  }
+  else{
+    this.frameTimeAnimation = this.frameTimeAnimation - this.speedAnimation*dt;
+    if(this.frameTimeAnimation < 0){
+      this.frameTimeAnimation = 12;
+      this.animationState = this.animationState + 1;
+    }
+  }*/
+
+  this.pose = this.pose + this.speedAnimation * dt;
+  if(this.atacando === 0){
+    if(this.vx === 0 && this.vy === 0){     // Personagem parado a pose se mantem
+      this.pose = 0;      
+    }
+  }
+  /*else{
+    
+  }*/
+
+  if(this.cooldownAtaque < 0){
+    this.atacando = 0;
+  }
+  else {
+    // Retorna pra animação inicial se o ataque já tiver concluido o ciclo
+    if(this.pose > this.animation[this.atacando].animationPosition[this.sentidoMovimento].qtdAnimacoes){
+      this.pose = 0;
+    }
+  }
+  
+  this.cooldownAtaque = this.cooldownAtaque - 2 * dt;
+}
+
+
+Player.prototype.teste = function(){
+  console.log(this.animation[this.atacando].animationPosition[this.sentidoMovimento]);
 }
 
 Player.prototype.desenhar = function(ctx){
+  let auxAnimation = this.animation[this.atacando].animationPosition[this.sentidoMovimento];  // Dados da animação do personagem
+  // tipo de animação -- sentido de movimento -- vetor de frames de animação
+
   let elipse = {
-    x: -this.sizeImagem/8 + 8,
-    y: -this.sizeImagem/16 + 3,
-    radiusX: this.sizeImagem/4 - 2,
-    radiusY: this.sizeImagem/8 - 2,
+    x: -auxAnimation.sw/8 + 8,
+    y: -auxAnimation.sh/16 + 3,
+    radiusX: auxAnimation.sw/4 - 2,
+    radiusY: auxAnimation.sh/8 - 2,
     rotation: 0,
     startAngle: 0,
     endAngle: 2 * Math.PI,
@@ -233,10 +320,18 @@ Player.prototype.desenhar = function(ctx){
   ctx.fill();
   ctx.stroke();
   //ctx, key, sx, sy, w, h, dx, dy, dw, dh
-  assetsMng.drawClipSize({ctx: ctx, key: this.nomeImagem, 
+  /*assetsMng.drawClipSize({ctx: ctx, key: this.nomeImagem, 
     sx: (this.sizeImagem * (this.animationState % this.qtdAnimacoes)),
     sy: (this.sizeImagem * this.pose), w: this.sizeImagem, h: this.sizeImagem, 
     dx: (- this.sizeImagem/2 ), dy: (5 - this.sizeImagem),  dw: this.sizeImagem, dh: this.sizeImagem
+  });*/
+  
+  assetsMng.drawClipSize({ctx: ctx, key: this.nomeImagem, 
+    sx: (auxAnimation.animationFrame[(Math.floor(this.pose) % auxAnimation.qtdAnimacoes)].sx),
+    sy: (auxAnimation.animationFrame[(Math.floor(this.pose) % auxAnimation.qtdAnimacoes)].sy),
+    w: auxAnimation.animationFrame[(Math.floor(this.pose) % auxAnimation.qtdAnimacoes)].sw, 
+    h: auxAnimation.animationFrame[(Math.floor(this.pose) % auxAnimation.qtdAnimacoes)].sh, 
+    dx: (- auxAnimation.sw/2), dy: (5 - auxAnimation.sh),  dw: auxAnimation.sw, dh: auxAnimation.sh
   });
   ctx.restore();
   if(debugMode == 1){
@@ -293,30 +388,6 @@ Player.prototype.desenharCaixaColisao = function(ctx){
     }*/
   }
 }
-
-Player.prototype.animationController = function(){
-  if(this.typeAnimation == 0){
-    if(this.vx !== 0 || this.vy !== 0){
-      this.frameTimeAnimation = this.frameTimeAnimation - this.speedAnimation*dt;
-      if(this.frameTimeAnimation < 0){
-        this.frameTimeAnimation = 12;
-        this.animationState = this.animationState + 1;
-      }
-    }
-    else{
-      this.frameTimeAnimation = 12;
-      this.animationState = 0;
-    }
-  }
-  else{
-    this.frameTimeAnimation = this.frameTimeAnimation - this.speedAnimation*dt;
-    if(this.frameTimeAnimation < 0){
-      this.frameTimeAnimation = 12;
-      this.animationState = this.animationState + 1;
-    }
-  }
-}
-
 /**
  * Mover que usa WIDTH e HEIGHT como referência no movimento
  */
